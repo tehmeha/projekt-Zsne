377802d174fe0d0fe67900662bcb489193c4a3d4
Zsne <dragan.cmarko@gmail.com>
2017-11-02 11:23:25 +0100

    1 dio

--- program.cpp
+++ program.cpp
@@ -2,5 +2,25 @@
 using namespace std;
 int main()
 {
+    cout << "1. Zbrajanje";
+    cout << "2. Oduzimanje";
+    cout << "3. Mnozenje";
+    cout << "4. Dijeljenje";
+    cout << "5. Korjenovanje";
+    cout << "6. Potenciranje";
+    cout << "7. Logortmiranje";
+    cout << "8. Trigonometrijske funkcije";
+    cout << "Upisite broj s izbornika: ";
+    int izbor;
+    cin >> izbor;
+    if( izbor == 1)
+    {
+        int a,b;
+
+    }
+    else if( izbor == 2)
+    {
+
+    }
 
 }
