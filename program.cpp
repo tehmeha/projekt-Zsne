7bd1c521586bc7ded9ca23363b3c98f526d4acb4
Zsne <dragan.cmarko@gmail.com>
2017-11-16 11:25:36 +0100

    radi

--- program.cpp
+++ program.cpp
@@ -1,26 +1,99 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 int main()
 {
-    cout << "1. Zbrajanje";
-    cout << "2. Oduzimanje";
-    cout << "3. Mnozenje";
-    cout << "4. Dijeljenje";
-    cout << "5. Korjenovanje";
-    cout << "6. Potenciranje";
-    cout << "7. Logortmiranje";
-    cout << "8. Trigonometrijske funkcije";
-    cout << "Upisite broj s izbornika: ";
+    cout << " 1.Zbrajanje: " << endl;
+    cout << " 2.Oduzimanje: " << endl;
+    cout << " 3.Mnozenje: " << endl;
+    cout << " 4.Dijeljenje: " << endl;
+    cout << " 5.Korjenovanje: " << endl;
+    cout << " 6.Potenciranje: " << endl;
+    cout << " 7.Logortmiranje: " << endl;
+    cout << " 8.Trigonometrijske funkcije: " << endl;
+    cout << endl;
+    cout << "Upisite broj s izbornika: " << endl;
     int izbor;
     cin >> izbor;
     if( izbor == 1)
     {
-        int a,b;
-
+        double a,b;
+        cout << "Unesite prvi broj: " << endl;
+        cin >> a;
+        cout << "Unesite drugi broj: " << endl;
+        cin >> b;
+        cout << a + b;
     }
     else if( izbor == 2)
+
     {
+        double a,b;
+        cout << "Unesite prvi broj: " << endl;
+        cin >> a;
+        cout << "Unesite drugi broj: " << endl;
+        cin >> b;
+        cout << a - b;
+    }
+    else if( izbor == 3)
+
+    {
+        double a,b;
+        cout << "Unesite prvi broj: " << endl;
+        cin >> a;
+        cout << "Unesite drugi broj: " << endl;
+        cin >> b;
+        cout << a * b;
+    }
+    else if( izbor == 4)
+
+    {
+        double a,b;
+        cout << "Unesite prvi broj: " << endl;
+        cin >> a;
+        cout << "Unesite drugi broj: " << endl;
+        cin >> b;
+        cout << a / b;
+    }
+    else if( izbor == 5)
+    {
+        double a,b;
+        cout << "Unesite broj: " << endl;
+        cin >> a;
+        cout << sqrt(a);
+    }
+     else if( izbor == 6)
+
+    {
+        double a,b;
+        cout << "Unesite prvi broj: " << endl;
+        cin >> a;
+        cout << "Unesite potenciju: " << endl;
+        cin >> b;
+        cout << pow(a,b);
+    }
+    else if( izbor == 7)
+
+    {
+        double a,b;
+        cout << "Unesite bazu: " << endl;
+        cin >> a;
+        cout << "Unesite argument: " << endl;
+        cin >> b;
+        cout << log10(a)/log10(b);
 
     }
+    else if( izbor == 8)
 
+    {
+        double a,b,c;
+        cout << "Unesite argument sinusa: " << endl;
+        cin >> a;
+        cout << sin(a * 3.14149265/180);
+        cout << "Unesite argument cosinusa: " << endl;
+        cin >> b;
+        cout << cos(b * 3.14149265/180);
+        cout << "Unesite argument tangesa " << endl;
+        cin >> c;
+        cout << tan(c * 3.14149265/180);
+    }
 }
